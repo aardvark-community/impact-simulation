ec4dbbc1286014205bf12d4a9bad3e9be7794389
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-03-09 16:32:13 +0100

    init

---
+++ build.cmd
@@ -0,0 +1,38 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+PUSHD %~dp0
+
+
+
+if exist boot.fsx (
+    if NOT exist .paket\fake.exe (
+        dotnet tool install fake-cli --tool-path .paket --version 5.16.0
+        if errorlevel 1 (
+          exit /b %errorlevel%
+        )
+    )
+
+    powershell write-host -fore Green bootstrapping project
+    .paket\fake.exe run boot.fsx
+    if errorlevel 1 (
+      exit /b %errorlevel%
+    )
+	del boot.fsx
+)
+
+IF NOT exist .paket\paket.exe (
+	dotnet tool install Paket --tool-path .paket
+)
+
+if NOT exist paket.lock (
+    echo No paket.lock found, running paket install.
+    .paket\paket.exe install
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+dotnet packages\build\fake-cli\tools\netcoreapp2.1\any\fake-cli.dll build %* 
+
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -f ".paket/paket" ]; then
+	echo installing paket
+	dotnet tool install Paket --tool-path .paket
+fi
+
+
+
+if [ -f boot.fsx ]; then
+	echo running boot
+	dotnet tool install fake-cli --tool-path .fakecli --version 5.16.0
+	.fakecli/fake run boot.fsx
+	rm boot.fsx
+	rm -dfr .fakecli
+fi
+
+if [ ! -f paket.lock ]; then
+	.paket/paket install
+fi
+
+.paket/paket restore
+dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@
+
+
