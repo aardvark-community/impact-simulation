8d03d9a0bbfa23569c73dc7d8d53bfa4b7283cb3
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-03-12 12:34:04 +0100

    fixed lookback

--- src/AardVolume/SimpleRenderer.fs
+++ src/AardVolume/SimpleRenderer.fs
@@ -37,6 +37,9 @@ module Shader =
             [<Semantic("CubeCoord")>]
             cubeCoord : V3d
 
+            
+            [<Semantic("CamModel")>]
+            camModel : V3d
         }
 
     let vertex (v : Vertex) =
@@ -47,9 +50,11 @@ module Shader =
                 pos = uniform.ViewProjTrafo * wp
                 dir = v.pos.XYZ - cameraInModel
                 cubeCoord = v.pos.XYZ
+                camModel = cameraInModel
             }
         }
 
+    // worst practice yet simple volume shader
     let fragment (v : Vertex) =
         fragment {
             let size = volumeTexture.Size
@@ -60,11 +65,14 @@ module Shader =
             let steps = 100
 
             let dir = -Vec.normalize v.dir / float steps
+            let mutable stop = false
 
             do
-                while sampleLocation.X >= 0.0 && sampleLocation.X <= 1.0 && sampleLocation.Y >= 0.0 && sampleLocation.Y <= 1.0 && sampleLocation.Z >= 0.0 && sampleLocation.Z <= 1.0 do
+                while sampleLocation.X >= 0.0 && sampleLocation.X <= 1.0 && sampleLocation.Y >= 0.0 && sampleLocation.Y <= 1.0 && sampleLocation.Z >= 0.0 && sampleLocation.Z <= 1.0 && not stop do
                     color <- color + V3d.III * volumeTexture.SampleLevel(sampleLocation, 0.0).X
                     sampleLocation <- sampleLocation + dir
+                    let viewPos = uniform.ModelViewProjTrafo * V4d(sampleLocation,1.0)
+                    if viewPos.Z / viewPos.W > 1.0 then stop <- true // stop looking backwards
 
             return V4d(2.0 * color / float steps, 1.0)
         }
@@ -75,7 +83,6 @@ module SimpleRenderer =
     
         Aardvark.Init()
 
-        // show the scene in a simple window
         let win = window {
             backend Backend.GL
             display Display.Mono
@@ -97,42 +104,10 @@ module SimpleRenderer =
         for layer in 0 .. images.Length - 1 do
             volume.Tensor4.SubImageAtZ(int64 layer).Set(images.[layer].Volume) |> ignore
 
-
         let texture = PixTexture3d(volume, false) :> ITexture
         let texture = win.Runtime.PrepareTexture(texture) :> ITexture |> AVal.constant
 
-
         let fvc = int64 volume.Size.X * int64 volume.Size.Y * int64 volume.Size.Z
-        let factor = fvc / (256L * 256L * 256L)
-
-        let drawCall = DrawCallInfo(FaceVertexCount = (fvc / factor |> int), InstanceCount = 1)
-
-        let blendMode = 
-            BlendMode(
-               Operation = BlendOperation.Add,
-               AlphaOperation = BlendOperation.Add,
-               SourceFactor = BlendFactor.One,
-               DestinationFactor = BlendFactor.One,
-               SourceAlphaFactor =BlendFactor.One,
-               DestinationAlphaFactor = BlendFactor.One,
-               Enabled = true
-            )
-
-        let signature =
-            win.Runtime.CreateFramebufferSignature [
-                DefaultSemantic.Colors, RenderbufferFormat.R32f
-            ]
-
-        let scatterTexture = win.Runtime.CreateTexture(V2i(256,1024), TextureFormat.R32f, 1, 1)
-
-        let fbo = 
-            win.Runtime.CreateFramebuffer(
-                signature, 
-                Map.ofList [
-                    DefaultSemantic.Colors, ({ texture = scatterTexture; slice = 0; level = 0 } :> IFramebufferOutput)
-                ]
-            )
-
 
         let size = V3d volume.Size / float volume.Size.NormMax
 
